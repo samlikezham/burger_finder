1b130a367f34d78fb2b0c924e017facaea40187e
Sam <schung015@gmail.com>
2018-08-24 00:59:03 -0700

    minor changes

---
+++ controllers/comments.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/comment')
+
+
+router.get('/comments', (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  // body parser lets us use the req.body
+  const { author, text } = req.body;
+  if (!author || !text) {
+    // we should throw an error. we can do this check on the front end
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
